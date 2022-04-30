8a0c536bb26cd8f749d672867b18d365cb1d3fc3
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-04-30 18:27:55 +0000

    move scripts to functions

--- footer.php
+++ footer.php
@@ -26,19 +26,7 @@
  <div id="preloader"></div>
  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
- <!-- Vendor JS Files -->
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/purecounter/purecounter.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/aos/aos.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/glightbox/js/glightbox.min.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/swiper/swiper-bundle.min.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/typed.js/typed.min.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/waypoints/noframework.waypoints.js"></script>
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/vendor/php-email-form/validate.js"></script>
-
- <!-- Template Main JS File -->
- <script src="<?= get_template_directory_uri(); ?>/themes/resume/assets/js/main.js"></script>
+ <?php wp_footer(); ?>
 
  </body>
 
--- functions.php
+++ functions.php
@@ -15,15 +15,15 @@ add_action('wp_enqueue_scripts', 'drl_theme_register_styles');
 
 function drl_theme_register_scripts()
 {
-    // wp_enqueue_script('purecounter', get_template_directory_uri() . '/themes/resume/assets/vendor/purecounter/purecounter.js', array(), false, true);
-    // wp_enqueue_script('aos', get_template_directory_uri() . '/themes/resume/assets/vendor/aos/aos.js', array(), false, true);
-    // wp_enqueue_script('bootstrap', get_template_directory_uri() . '/themes/resume/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array(), false, true);
-    // wp_enqueue_script('glightbox', get_template_directory_uri() . '/themes/resume/assets/vendor/glightbox/js/glightbox.min.js', array(), false, true);
-    // wp_enqueue_script('isotope', get_template_directory_uri() . '/themes/resume/assets/vendor/isotope-layout/isotope.pkgd.min.js', array(), false, true);
-    // wp_enqueue_script('swiper', get_template_directory_uri() . '/themes/resume/assets/vendor/swiper/swiper-bundle.min.js', array(), false, true);
-    // wp_enqueue_script('typed', get_template_directory_uri() . '/themes/resume/assets/vendor/typed.js/typed.min.js', array(), false, true);
-    // wp_enqueue_script('noframework', get_template_directory_uri() . '/themes/resume/assets/vendor/waypoints/noframework.waypoints.js', array(), false, true);
-    // wp_enqueue_script('validate', get_template_directory_uri() . '/themes/resume/assets/vendor/php-email-form/validate.js', array(), false, true);
-    // wp_enqueue_script('main', get_template_directory_uri() . '/themes/resume/assets/js/main.js', array(), false, true);
+    wp_enqueue_script('purecounter', get_template_directory_uri() . '/themes/resume/assets/vendor/purecounter/purecounter.js', array(), false, true);
+    wp_enqueue_script('aos', get_template_directory_uri() . '/themes/resume/assets/vendor/aos/aos.js', array(), false, true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/themes/resume/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array(), false, true);
+    wp_enqueue_script('glightbox', get_template_directory_uri() . '/themes/resume/assets/vendor/glightbox/js/glightbox.min.js', array(), false, true);
+    wp_enqueue_script('isotope', get_template_directory_uri() . '/themes/resume/assets/vendor/isotope-layout/isotope.pkgd.min.js', array(), false, true);
+    wp_enqueue_script('swiper', get_template_directory_uri() . '/themes/resume/assets/vendor/swiper/swiper-bundle.min.js', array(), false, true);
+    wp_enqueue_script('typed', get_template_directory_uri() . '/themes/resume/assets/vendor/typed.js/typed.min.js', array(), false, true);
+    wp_enqueue_script('noframework', get_template_directory_uri() . '/themes/resume/assets/vendor/waypoints/noframework.waypoints.js', array(), false, true);
+    wp_enqueue_script('validate', get_template_directory_uri() . '/themes/resume/assets/vendor/php-email-form/validate.js', array(), false, true);
+    wp_enqueue_script('main-theme', get_template_directory_uri() . '/themes/resume/assets/js/main.js', array(), false, true);
 }
 add_action('wp_enqueue_scripts', 'drl_theme_register_scripts');
